5101cb4c13e1e7265fb09f1f6548f0dd3c7678fc
Taron Grigoryan <1991taron@gmail.com>
2022-07-18 09:15:50 +0400

    Add project files.

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirsWebApp.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirsWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
